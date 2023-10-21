ad0e8d9848ee6ddf05835ef2ca51b9f18f2e618c
Emile Reese <emilereeseii@gmail.com>
2023-10-21 01:46:34 +0000

    Modified SlidingQueue to become Immutable

--- src/main/java/edu/luc/cs/consoleapp/SlidingQueue.java
+++ src/main/java/edu/luc/cs/consoleapp/SlidingQueue.java
@@ -5,21 +5,47 @@ import java.util.stream.Stream;
 
 import org.apache.commons.collections4.queue.CircularFifoQueue;
 
+
+
 class SlidingQueue {
 
-  protected final Queue<String> queue;
+  private final Queue<String> queue;
 
-  public SlidingQueue(final int queueSize) {
+  private SlidingQueue(final int queueSize) {
     this.queue = new CircularFifoQueue<>(queueSize);
   }
 
-  public void process(final Stream<String> input, final OutputObserver output) {
-    input
-        .takeWhile(
-            word -> {
-              queue.add(word); // the oldest item automatically gets evicted
-              return output.test(queue);
-            })
-        .count(); // forces evaluation of the entire stream
+   public SlidingQueue process(final Stream<String> input, final OutputObserver output) {
+    Stream<String> transformedInput = transformWithMapAndScanLeft(input);
+
+    int queueCapacity = 5; 
+    Stream<Queue<String>> slidingQueueInput = transformToSlidingQueue(transformedInput, queueCapacity);
+
+    Stream<Integer> longestWordLengths = transformToLongestWordLengths(slidingQueueInput);
+
+    return this; 
+  }
+
+  private Stream<String> transformWithMapAndScanLeft(Stream<String> input) {
+    return input.map(s -> s.toUpperCase()).scanLeft("", (acc, item) -> acc + item);
+  }
+
+  private Stream<Queue<String>> transformToSlidingQueue(Stream<String> input, int capacity) {
+    SlidingQueue slidingQueue = new SlidingQueue(capacity);
+    return input.scanLeft(slidingQueue, (queue, item) -> {
+      queue.add(item);
+      return queue;
+    });
+  }
+
+  private Stream<Integer> transformToLongestWordLengths(Stream<Queue<String>> input) {
+    return input.map(queue -> {
+      int maxWordLength = 0;
+      for (String word : queue) {
+        maxWordLength = Math.max(maxWordLength, word.length());
+      }
+      return maxWordLength;
+    });
   }
 }
+
